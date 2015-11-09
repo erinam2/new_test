92b4471c916d39faaf79770edf6eb76d9d878d12
erinam2 <erina-ema@yandex.ru>
2015-11-09 10:15:22 +0300

    Создан AddEmployeeTransaction и  тесты

---
+++ ConsoleApplication1/NewClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class NewClass
+    {
+        private int rr;
+        public NewClass(int _rr)
+        {
+            rr = _rr;
+        }
+    }
+}
